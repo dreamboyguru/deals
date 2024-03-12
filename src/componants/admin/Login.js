a5ddcb55f2aaa636c3cd4d4c681e4972010038fe
Gurusidda Hanamannavr <143526836+dreamboyguru@users.noreply.github.com>
2024-03-12 14:39:58 +0530

    Update Login.js

--- src/componants/admin/Login.js
+++ src/componants/admin/Login.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import axios from 'axios';
@@ -6,6 +6,7 @@ import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
     const navigate = useNavigate();
+    const [error, setError] = useState('');
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="max-w-md w-full p-8 bg-white shadow-lg rounded-lg">
@@ -23,12 +24,14 @@ const Login = () => {
           onSubmit={async(values, { setSubmitting }) => {
             console.log(values);
                 try {
-                const response = await axios.post('https://dealsbackend.onrender.com/login', values);
+                const response = await axios.post('http://localhost:3001/login', values);
                     console.log('Login successful:', response.data);
                     localStorage.setItem('email', response.data)
-                    if(response.data) {
+                    if(response.data !== 'not found' ) {
                         navigate('/')
                         window.location.reload();
+                    } else {
+                        setError('Invalid email & password')
                     }
                 } catch (error) {
                 console.log(error);
@@ -40,6 +43,7 @@ const Login = () => {
           {({ isSubmitting }) => (
             <Form>
               <div className="mb-4">
+                <div className='text-red-600'>{error}</div>
                 <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email Address</label>
                 <Field type="email" name="email" className="mt-1 block w-full rounded-md border-gray-300 py-2 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
                 <ErrorMessage name="email" component="div" className="text-red-500 text-sm mt-1" />
@@ -49,7 +53,7 @@ const Login = () => {
                 <Field type="password" name="password" className="mt-1 block w-full rounded-md border-gray-300 py-2 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
                 <ErrorMessage name="password" component="div" className="text-red-500 text-sm mt-1" />
               </div>
-              <button type="submit" disabled={isSubmitting} className="w-full bg-indigo-500 text-white py-2 px-4 rounded-md hover:bg-indigo-600 focus:outline-none focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+              <button disabled={isSubmitting} className="w-full bg-indigo-500 text-white py-2 px-4 rounded-md hover:bg-indigo-600 focus:outline-none focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
                 {isSubmitting ? 'Submitting...' : 'Submit'}
               </button>
             </Form>
